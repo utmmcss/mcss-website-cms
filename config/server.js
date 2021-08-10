8435dfc9ffc7c26659d242887c7b09873b8143af
TheFatPanda97 <shawnhu97@gmail.com>
2021-08-10 18:10:39 -0400

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fa7d41d0886591d35202c2be7dd42277'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a1186aef-b11d-465d-9a6d-a420e02d21cd'
+};
