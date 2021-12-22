3b30a8e4882f8666a5a0819e827ad81a6d30c061
TheFatPanda97 <shwnhu97@gmail.com>
2021-12-21 21:06:48 -0500

    updated img secrity policy

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,16 @@
-module.exports = [
+module.exports = ({ env }) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': ["'self'", 'data:', 'https:', 'cdn.jsdelivr.net', 'strapi.io'],
+        },
+      }
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
