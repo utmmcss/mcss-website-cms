0ead125d9ca98bf913cf233eadf05b1b3d2be690
Razeen Ali <43294516+r4z33n4l1@users.noreply.github.com>
2023-10-01 16:31:30 -0500

    MCSS - temporary PR to merge branch to issue number (#5)
    Co-authored-by: Khoi Nguyen <bob.k.nguyen@gmail.com>

---
+++ src/api/academic/controllers/academic.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ *  academic controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::academic.academic', ({ strapi }) => ({
+  async find(ctx) {
+    var { data, meta } = await super.find(ctx);
+
+    data = data.map(item => {
+      const newItem = {...item};
+      if (newItem.attributes) {
+        delete newItem.attributes.Content;
+        delete newItem.attributes.Creator;
+
+        for (const key in newItem.attributes) {
+          if (key.charAt(0).toLowerCase() !== key.charAt(0)) {
+            newItem.attributes[key.charAt(0).toLowerCase() + key.slice(1)] = newItem.attributes[key];
+            delete newItem.attributes[key];
+          }
+        }
+      }
+      return newItem;
+    });
+
+    return { data, meta };
+  },
+
+  async findOne(ctx) {
+    var response = await super.findOne(ctx);
+
+    for (const key in response.data.attributes) {
+          if (key.charAt(0).toLowerCase() !== key.charAt(0)) {
+            response.data.attributes[key.charAt(0).toLowerCase() + key.slice(1)] = response.data.attributes[key];
+            delete response.data.attributes[key];
+          }
+    }
+    return response;
+  }
+}));
---
+++ src/api/blog-category/controllers/blog-category.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ *  blog-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::blog-category.blog-category', ({ strapi }) => ({
+  async find(ctx) {
+    var { data, meta } = await super.find(ctx);
+
+    data = data.map(item => {
+      const newItem = {...item};
+      if (newItem.attributes) {
+        for (const key in newItem.attributes) {
+          if (key.charAt(0).toLowerCase() !== key.charAt(0)) {
+            newItem.attributes[key.charAt(0).toLowerCase() + key.slice(1)] = newItem.attributes[key];
+            delete newItem.attributes[key];
+          }
+        }
+      }
+      return newItem;
+    });
+
+    return { data, meta };
+  },
+
+  async findOne(ctx) {
+    var response = await super.findOne(ctx);
+
+    for (const key in response.data.attributes) {
+          if (key.charAt(0).toLowerCase() !== key.charAt(0)) {
+            response.data.attributes[key.charAt(0).toLowerCase() + key.slice(1)] = response.data.attributes[key];
+            delete response.data.attributes[key];
+          }
+    }
+    return response;
+  }
+}));
