16814262e67ebd8511b0087d7b4a559439e7bb80
Khoi Nguyen <bob.k.nguyen@gmail.com>
2023-10-10 00:26:36 -0400

    Fix capitalization

--- src/api/event-category/controllers/event-category.js
+++ src/api/event-category/controllers/event-category.js
@@ -6,4 +6,35 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::event-category.event-category');
+module.exports = createCoreController('api::event-category.event-category', ({ strapi }) => ({
+  async find(ctx) {
+    var { data, meta } = await super.find(ctx);
+
+    data = data.map(item => {
+      const newItem = {...item};
+      if (newItem.attributes) {
+        for (const key in newItem.attributes) {
+          if (key.charAt(0).toLowerCase() !== key.charAt(0)) {
+            newItem.attributes[key.charAt(0).toLowerCase() + key.slice(1)] = newItem.attributes[key];
+            delete newItem.attributes[key];
+          }
+        }
+      }
+      return newItem;
+    });
+
+    return { data, meta };
+  },
+
+  async findOne(ctx) {
+    var response = await super.findOne(ctx);
+
+    for (const key in response.data.attributes) {
+          if (key.charAt(0).toLowerCase() !== key.charAt(0)) {
+            response.data.attributes[key.charAt(0).toLowerCase() + key.slice(1)] = response.data.attributes[key];
+            delete response.data.attributes[key];
+          }
+    }
+    return response;
+  }
+}));
--- src/api/partner/controllers/partner.js
+++ src/api/partner/controllers/partner.js
@@ -6,4 +6,38 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::partner.partner');
+module.exports = createCoreController("api::partner.partner", ({ strapi }) => ({
+    async find(ctx) {
+      var { data, meta } = await super.find(ctx);
+
+      // Don't include Content and Author attributes when querying for all partners
+      data = data.map(item => {
+        const newItem = { ...item };
+        if (newItem.attributes) {
+          delete newItem.attributes.Content;
+          for (const key in newItem.attributes) {
+            if (key.charAt(0).toLowerCase() !== key.charAt(0)) {
+              newItem.attributes[key.charAt(0).toLowerCase() + key.slice(1)] = newItem.attributes[key];
+              delete newItem.attributes[key];
+            }
+          }
+        }
+        return newItem;
+      });
+
+      return { data, meta };
+    },
+
+    async findOne(ctx) {
+        var response = await super.findOne(ctx);
+
+        for (const key in response.data.attributes) {
+          if (key.charAt(0).toLowerCase() !== key.charAt(0)) {
+            response.data.attributes[key.charAt(0).toLowerCase() + key.slice(1)] = response.data.attributes[key];
+            delete response.data.attributes[key];
+          }
+      }
+
+        return response;
+      }
+  }));
